f3c140268489677158f10ecdab285d74d7cd161d
munichpavel <munichpavel@gmail.com>
2020-04-04 11:48:56 +0200

    Add resource allocation value validation

--- src/team_planning_for_geeks/planning.py
+++ src/team_planning_for_geeks/planning.py
@@ -4,22 +4,58 @@ import numpy as np
 import xarray as xr
 
 @attr.s
+class ValidateBounds:
+    """
+    Validate that values satisfy instance bounds
+    
+    Attributes
+    ----------
+    lower_bound : float
+    upper_bound : float
+    """
+    lower_bound = attr.ib()
+    upper_bound = attr.ib()
+    @upper_bound.validator
+    def check(self, attribute, value):
+        if value < self.lower_bound:
+            raise ValueError('Upper bound may not be less than lower')
+    
+    def __call__(self, values):
+        """Wrapper to validate arguments of f"""
+
+        if not self.all_values_geq_lower_bound(values):
+            raise ValueError(f"Some values below {self.lower_bound}")
+        if not self.all_values_leq_upper_bound(values):
+            raise ValueError(f"Some values above {self.upper_bound}")
+
+    def all_values_geq_lower_bound(self, values):
+        return np.all(values >= self.lower_bound)
+
+    def all_values_leq_upper_bound(self, values):
+        return np.all(values <= self.upper_bound)
+
+validator = ValidateBounds(0., 1.)
+@attr.s
 class Planner:
     names=attr.ib()
     tasks=attr.ib()
     time=attr.ib()
 
-    def initialize_values(self, value=0):
+    def initialize_values(self, value=0.):
         full_values = np.full((len(self.names), len(self.tasks), len(self.time)), value)
         self._data = self.set_data(full_values)
         self.values = self._get_values()
     
     def set_data(self, values):
-        return xr.DataArray(
+        validator(values)
+        res = xr.DataArray(
             values,
             dims=('name', 'task', 'time'),
             coords=dict(name=self.names, task=self.tasks, time=self.time)
         )
+        return res
 
     def _get_values(self):
         return self._data.values
+
+    
--- tests/test_planning.py
+++ tests/test_planning.py
@@ -1,7 +1,27 @@
+import pytest
 import numpy as np
-from team_planning_for_geeks.planning import Planner
+from team_planning_for_geeks.planning import Planner, ValidateBounds
 
-def test_init():
+
+def test_validate_bounds():
+    values = np.array([0., 1., 2.])
+    ValidateBounds(0., 2.)(values)
+
+    with pytest.raises(ValueError):
+        ValidateBounds(1., -1)
+
+    with pytest.raises(ValueError):
+        ValidateBounds(0., 1.)(values)
+
+    with pytest.raises(ValueError):
+        ValidateBounds(1., 2.)(values)
+
+
+def test_initialize():
     planner = Planner(['harry', 'ron', 'hermione'], ['potions', 'herbology'], range(12))
-    planner.initialize_values(0)
+    planner.initialize_values(0.)
     np.testing.assert_array_equal(planner.values, np.zeros((3,2,12)))
+
+    with pytest.raises(ValueError):
+        planner.initialize_values(1.5)
+        planner.initialize_values(-1)
